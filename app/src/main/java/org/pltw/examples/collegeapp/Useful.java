6801fe4adc27123abc1c7ee2841d1b95aa387710
Mr. Shorr <gshorr@spusd.net>
2016-01-05 15:08:09 -0800

    oops

--- app/src/main/java/org/pltw/examples/collegeapp/Useful.java
+++ app/src/main/java/org/pltw/examples/collegeapp/Useful.java
@@ -3,6 +3,6 @@ package org.pltw.examples.collegeapp;
 /**
  * Created by gshorr on 1/5/16.
  */
-public class Useless {
+public class Useful {
     public static final String STUPID_VARIABLES = "POINTLESS";
 }
