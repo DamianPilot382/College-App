83b2e145fb9f25134244318230f8ae23be6a6db9
Mr. Shorr <gshorr@spusd.net>
2016-01-05 15:10:44 -0800

    oops

--- app/src/main/java/org/pltw/examples/collegeapp/ApplicantActivity.java
+++ app/src/main/java/org/pltw/examples/collegeapp/ApplicantActivity.java
@@ -24,6 +24,7 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 //some comment goes here.
+//fine i'll go here
 
 public class ApplicantActivity extends ActionBarActivity {
 
